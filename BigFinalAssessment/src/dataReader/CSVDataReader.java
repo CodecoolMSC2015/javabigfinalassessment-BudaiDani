6f6cbbb8862ef8ea2c0f9620d6b3f1ca092c5b32
Budai Dani <budai.d945@gmail.com>
2016-03-16 15:51:59 +0100

    buffered reader used in csvdatareader

--- BigFinalAssessment/src/dataReader/CSVDataReader.java
+++ BigFinalAssessment/src/dataReader/CSVDataReader.java
@@ -4,40 +4,74 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import person.Person;
 
 public class CSVDataReader extends DataReader
 {
-	static String csvFilePath = ".\\Documentation\\persons.csv";
-	public static void main(String[] args)
+	String csvFilePath;
+	List<Person> persons;
+
+	public CSVDataReader()
 	{
+		BufferedReader reader = null;
+		try
+		{
+			reader = new BufferedReader(new FileReader("\\Documentation\\persons.csv"));
+		}
+		catch (FileNotFoundException e2)
+		{
+			e2.printStackTrace();
+		}
+
+		String line = null;
+		Scanner scanner = null;
+		int index = 0;
+		List<Person> personList = new ArrayList<>();
 
 		try
 		{
-			BufferedReader br = new BufferedReader(new FileReader(csvFilePath));
-			String line = null;
-			try
+			while ((line = reader.readLine()) != null)
 			{
-				line = br.readLine();
+				Person per = new Person();
+				scanner = new Scanner(line);
+				scanner.useDelimiter(",");
+				while (scanner.hasNext())
+				{
+					String data = scanner.next();
+					if (index == 0)
+						per.setName((data));
+					else if (index == 1)
+						per.setEmail(data);
+					else if (index == 2)
+						System.out.println(data);
+				}
+				index = 0;
+				personList.add(per);
 			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-			String[] row = line.split(" ");
-
 		}
-		catch (FileNotFoundException e)
+		catch (IOException e1)
+		{
+			e1.printStackTrace();
+		}
+
+		try
+		{
+			reader.close();
+		}
+		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
 	}
 
-	public Set<Person> getPersons()
+	public static void main(String[] args) throws IOException
 	{
-		return null;
-
+		new CSVDataReader();
 	}
+
 }
+
